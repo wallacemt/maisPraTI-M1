b2e591d423b787ae79fcbe5b3a1086be992ea1f6
Wallace Santana <wallacesantanak0@gmail.com>
2024-08-19 16:23:47 -0300

    Listas duplamente encadeads

--- modulo-01/lesson14/index.js
+++ modulo-01/lesson14/index.js
@@ -1,3 +1,5 @@
+//Listas encadeadas
+
 class Node {
     constructor(data) {
         this.data = data //O dado armazenado pelo nó
@@ -12,44 +14,43 @@ class LinkedList {
     }
     //adiciona um novo nó com o dado 'data' ao final da lista
     add(data) {
-        let newNode = new Node(data)
+        let newNode = new Node(data)//O novo nó a ser adicionado
 
         if (this.head === null) {
-            this.head = newNode
+            this.head = newNode//O primeiro nó da lista
         } else {
-            let current = this.head
+            let current = this.head//O primeiro nó da lista
             while (current.next !== null) {
-                current = current.next
+                current = current.next//O proximo nó da lista
             }
-
-            current.next = newNode
+            current.next = newNode//O proximo nó da lista
         }
-        this.size++
+        this.size++//O tamanho da lista
     }
 
     //Insere um elemento em qualquer posição da lista
     insertAt(data, index) {
         if (index < 0 || index > this.size) {
-            return console.log('Index fora dos limites!')
+            return console.log('Index fora dos limites!')//index fora dos limites
         }
 
-        let newNode = new Node(data)
-        let current = this.head
-        let previous
+        let newNode = new Node(data)//O novo nó a ser adicionado
+        let current = this.head//O primeiro nó da lista
+        let previous//O nó anterior
 
         if (index === 0) {
-            newNode.next = this.head
-            this.head = newNode
+            newNode.next = this.head//O proximo nó da lista
+            this.head = newNode//O primeiro nó da lista
         } else {
             for (let i = 0; i < index; i++) {
-                previous = current
-                current = current.next
+                previous = current//O nó anterior
+                current = current.next//O proximo nó
             }
-            newNode.next = current
-            previous.next = newNode
+            newNode.next = current//O proximo nó da lista
+            previous.next = newNode//O proximo nó da lista
         }
 
-        this.size++
+        this.size++//O tamanho da lista
     }
     removeFrom(index){
         if(index < 0 || index > this.sizeOfList){
@@ -60,14 +61,14 @@ class LinkedList {
         let previous
 
         if(index === 0){
-            this.head = current.next
+            this.head = current.next 
         }else{
             for(let i = 0; i < index; i++){
                 previous = current
                 current = current.next
             }
-            this.size = this.size - 1
-            return current.data
+            this.size = this.size - 1 //O tamanho da lista
+            return current.data //Retorna o dado
         }
     }
 
@@ -76,37 +77,38 @@ class LinkedList {
         let index = 0
         while(current !== null){
             if(current.data === data){
-                return index
+                return index//Retorna o indice
             }
-            index++
-            current = current.next
+            index++//Incrementa o indice
+            current = current.next//O proximo nó
         }
         return -1
     }
 
     sizeOfList(){
-        return this.size
+        return this.size //Retorna o tamanho
     }
     printList() {
-        let current = this.head
+        let current = this.head//O primeiro nó
         while (current !== null) {
-            console.log(current.data)
-            current = current.next
+            console.log(current.data)//Imprime o dado
+            current = current.next//O proximo nó
         }
     }
 }
 
-let list = new LinkedList();
+let list = new LinkedList(); //Cria uma nova lista
 
-list.add("Elemento 0")
+list.add("Elemento 0")//Adiciona um novo elemento
 list.add("Elemento 1")
 list.add("Elemento 2")
 list.add("Elemento 3")
-list.removeFrom(2)
 
-console.log(list.indexOf("Elemento 0"))
-list.printList()
+list.removeFrom(2)//Remove o elemento de indice 2
 
-console.log(list.sizeOfList())
+console.log(list.indexOf("Elemento 0")) //Retorna o indice
+list.printList() //Imprime a lista
+
+console.log(list.sizeOfList()) //Retorna o tamanho
 
 
